8be840b6184315480da1fce57d283da785743131
Steven Vondruska <vondruska@users.noreply.github.com>
2019-09-26 12:02:55 -0400

    Handling server shutdown

--- node-http/server.js
+++ node-http/server.js
@@ -1,10 +1,18 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
+// setup simple endpoint
 app.get('/', function (req, res) {
   res.send('RamHacks HTTP Server')
-})
+});
 
-app.listen(3000, () => {
+// startup server
+const server = app.listen(3000, () => {
     console.log("HTTP server running");
+});
+
+// handle shutdown from OS or terminal
+process.on('SIGINT', () => {
+  console.log('Shutting down');
+  server.close();
 });
